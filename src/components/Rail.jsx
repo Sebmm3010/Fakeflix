4404a4c7203627cf6963ec5818a0960033ea4766
Sebastian <rockmadero@gmail.com>
2022-12-09 17:57:34 -0500

    rails funcionando

--- src/components/Rail.jsx
+++ src/components/Rail.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import { getMovies } from "../services/getmovies";
 import { RailItem } from "./";
 
-export const Rail = ({ titulo = '', options = {} }) => {
+export const Rail = ({ titulo = '', options = {}, rowId='' }) => {
 
   const [movies, setMovies] = useState([]);
 
@@ -12,19 +13,36 @@ export const Rail = ({ titulo = '', options = {} }) => {
     })();
   }, [options]);
 
-  
+  const slideLeft = () => {
+    const slider = document.getElementById('slider'+rowId);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  }
+  const slideRight = () => {
+    const slider = document.getElementById('slider'+rowId);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  }
 
+  const sliders = "bg-white rounded-full absolute opacity-50 hove:opacity-100 cursor-pointer z-10 hidden group-hover:block"
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{titulo}</h2>
-      <div className="relative flex items-center">
-        <div id="slider">
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          className={sliders + ' left-0'}
+          size={40}
+          onClick={ slideLeft }
+        />
+        <div id={`slider${rowId}`} className="w-full h-full overflow-x-scroll whitespace-nowrap scrollbar-hide scroll-smooth relative">
           {
             movies?.map((movie) => (
-              <RailItem key={movie.id} movie={movie}/>
-            ))
-          }
+              <RailItem key={movie.id} movie={movie} />
+            ))}
         </div>
+        <MdChevronRight
+          className={sliders + ' right-0'}
+          size={40}
+          onClick={ slideRight }
+        />
       </div>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,12 @@ export const Home = () => {
   return (
     <>
       <Hero />
-      <Rail titulo='Proximamente' options={{opt:'soon'}} />
-      <Rail titulo='Populares' options={{ opt: 'popular', page: 2 }} />
-      <Rail titulo='Lo mejor del momento' options={{ opt: 'popular', page: 1 }} />
-      <Rail titulo='Lo mas votado' options={{ opt: 'top' }} />
-      <Rail titulo='Acción' options={{ opt: 'categoria', type: 'action' }} />
-      <Rail titulo='Horror' options={{ opt: 'categoria', type: 'horror' }} />
+      <Rail rowId='1' titulo='Proximamente' options={{opt:'soon'}} />
+      <Rail rowId='2' titulo='Populares' options={{ opt: 'popular', page: 2 }} />
+      <Rail rowId='3' titulo='Lo mejor del momento' options={{ opt: 'popular', page: 1 }} />
+      <Rail rowId='4' titulo='Lo mas votado' options={{ opt: 'top' }} />
+      <Rail rowId='5' titulo='Acción' options={{ opt: 'categoria', type: 'action' }} />
+      <Rail rowId='6' titulo='Horror' options={{ opt: 'categoria', type: 'horror' }} />
     </>
   )
 }
