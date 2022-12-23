81c58417cdf949566df8c1443edd56a71a50ad17
Sebastian <rockmadero@gmail.com>
2022-12-23 17:32:55 -0500

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
@@ -9,9 +9,11 @@ import { store } from './store/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
+    {/* </BrowserRouter> */}
   </Provider>
   // </React.StrictMode>
 )
